4cf06b76b8ad9e4f6ce04626257b6b22d95168b5
worawek <worawekch@gmail.com>
2018-05-26 15:17:12 +0700

    update

---
+++ pages/view.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <img :src="photo" alt="">
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            photo: '/icon.png'
+        }
+    },
+    created(){
+       let photo = window.localStorage.getItem('photo')
+       if (photo){
+           this.photo = photo
+       }
+    }
+}
+</script>
